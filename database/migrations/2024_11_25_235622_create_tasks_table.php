b053b0398993e07d2598bf5cf5fcbe4b1359701d
@Pret_ty <tchepretty@gmail.com>
2024-11-27 18:36:56 +0100

    Creation du projet, models, migrations

---
+++ database/migrations/2024_11_25_235622_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('status', ['non débuté', 'en cours', 'terminé'])->default('non commencé');
+            $table->enum('priority', ['non débuté', 'en cours', 'terminé'])->default('non commencé');
+            $table->enum('priority', ['urgente', 'pas urgente']);
+            $table->foreignId('project_id')->constrained()->onDelete('cascade');
+            $table->foreignId('assigned_to')->nullable()->constrained('users')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
